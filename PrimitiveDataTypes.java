901974e4ab38e178e697c8daca4e8fdb394c3bf3
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-13 20:25:22 +0530

    Create PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 1000000000L;
+        float f = 12.5f;
+        double d = 20.12345;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + bool);
+    }
+}
