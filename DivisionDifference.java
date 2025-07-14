7a97105286bbd022b847bc8d736552447523eeb4
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-14 20:52:03 +0530

    Create DivisionDifference.java

---
+++ DivisionDifference.java
@@ -0,0 +1,13 @@
+public class DivisionDifference {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+
+        int intResult = a / b;
+
+        double doubleResult = (double) a / b;
+
+        System.out.println("Integer division (7 / 2): " + intResult);
+        System.out.println("Double division (7 / 2): " + doubleResult); 
+    }
+}
