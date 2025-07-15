4df76d27a6cc808fb7437601c532eda2cce0d783
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-15 20:38:26 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double divide(int a, int b) {
+        if(b == 0) {
+            System.out.println("Error: Division by zero");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        int x = 20;
+        int y = 5;
+
+        System.out.println("Addition: " + calc.add(x, y));       // 25
+        System.out.println("Subtraction: " + calc.subtract(x, y)); // 15
+        System.out.println("Multiplication: " + calc.multiply(x, y)); // 100
+        System.out.println("Division: " + calc.divide(x, y));     // 4.0
+    }
+}
